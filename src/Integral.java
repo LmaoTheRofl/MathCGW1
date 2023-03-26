71ae1952651063f38d231cbef85be8dd73cf3e5a
LmaoTheRofl <Zharkov.nik04@mail.ru>
2023-03-26 17:35:57 +0300

    CycleUpdate

--- src/Integral.java
+++ src/Integral.java
@@ -6,7 +6,7 @@ public class Integral {
     float a; //начальная точка
     float b; //конечная точка
     int t; //способ выбора точек внутри промежутка: 1-правые, 2-левые, 3-средние, 4-случайные, 0-свое значение
-    // double j; параметр, определяющий положение точек внутри элементарных отрезков от 0 до 1
+    // double j;  параметр, определяющий положение точек внутри элементарных отрезков от 0 до 1
 
     public Integral(int n, float a, float b, int t) {
         this.n = n;
@@ -14,14 +14,28 @@ public class Integral {
         this.b = b;
         this.t = t;
     }
+    private static double getDoubleId(Scanner scanner) {
+        while (true) {
+            String s = scanner.nextLine();
+            try {
+                double l = Double.parseDouble(s);
+                if (l > 0) {
+                    return l;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Incorrect input!");
+            }
+        }}
+
     public double calculate() {
+        double[] arr = new double[n-1];
         if (n < 2) {
             System.out.println("Incorrect input");
             System.exit(1);
         }
         if (t == 1){
             double j = 0;
-            double[] arr = new double[n-1];
+
             for (int i = 0; i<arr.length; i++) {
                 arr[i] = Math.pow(Math.E, (a + i * (b-a)/(n-1) + j * (b-a)/(n-1)));
             }
@@ -29,7 +43,6 @@ public class Integral {
             return sum*(b-a)/(n-1);}
         else if (t == 2) {
             double j = 1;
-            double[] arr = new double[n-1];
             for (int i = 0; i<arr.length; i++) {
                 arr[i] = Math.pow(Math.E, (a + i * (b-a)/(n-1) + j * (b-a)/(n-1)));
             }
@@ -37,7 +50,6 @@ public class Integral {
             return sum*(b-a)/(n-1);}
         else if (t == 3){
             double j = 0.5;
-            double[] arr = new double[n-1];
             for (int i = 0; i<arr.length; i++) {
                 arr[i] = Math.pow(Math.E, (a + i * (b-a)/(n-1) + j * (b-a)/(n-1)));
             }
@@ -45,7 +57,6 @@ public class Integral {
             return sum*(b-a)/(n-1);}
         else if (t == 4) {
         double j = Math.random();
-            double[] arr = new double[n-1];
             for (int i = 0; i<arr.length; i++) {
                 arr[i] = Math.pow(Math.E, (a + i * (b-a)/(n-1) + j * (b-a)/(n-1)));
             }
@@ -54,16 +65,17 @@ public class Integral {
         else if (t == 0) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Введите своё значение j от 0 до 1");
-            double j = scanner.nextDouble();
+            double j =  getDoubleId(scanner);
             if (j < 0 || j > 1) {
                 System.out.println("Incorrect input");
-                System.exit(1);
-                double[] arr = new double[n-1];
+                System.exit(1); }
+            else {
                 for (int i = 0; i<arr.length; i++) {
                     arr[i] = Math.pow(Math.E, (a + i * (b-a)/(n-1) + j * (b-a)/(n-1)));
-                }
+                   }
                 double sum = DoubleStream.of(arr).sum();
-                return sum*(b-a)/(n-1);}
+                return sum*(b-a)/(n-1);
+                }
         }
         return 0;
     }
