1b4099131d8ab9436f3df029d6474bd1bde4a919
LmaoTheRofl <zharkov.nik04@mail.ru>
2023-04-07 19:26:38 +0300

    MainScannerUpdate

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Integral x = new Integral(2, 0, 1, 0);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество точек на интервале");
+        int n = scanner.nextInt();
+        Integral x = new Integral(n, 0, 1, 0);
         System.out.println(x.calculate());
     }
 }
