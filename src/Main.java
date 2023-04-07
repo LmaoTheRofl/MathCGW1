e19ae2f2a829ff131a2e293ea61753cc748b5100
LmaoTheRofl <zharkov.nik04@mail.ru>
2023-04-07 19:29:28 +0300

    ParamsUpdate

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество точек на интервале");
         int n = scanner.nextInt();
-        Integral x = new Integral(n, 0, 1, 0);
+        System.out.println("Введите способ выбора точек внутри промежутка: 1-правые, 2-левые, 3-средние, 4-случайные, 0-свое значение");
+        int t = scanner.nextInt();
+        Integral x = new Integral(n, 0, 1, t);
         System.out.println(x.calculate());
     }
 }
